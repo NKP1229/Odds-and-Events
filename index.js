2988789a15eecb2a44d56359c50f03b6642ea70a
NKP1229 <113551880+NKP1229@users.noreply.github.com>
2025-04-01 16:38:30 -0400

    push a number to NumberBank array and displayed on HTML

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // TODO: this file! :)
+const NumberBank = [];
+let numberBank;
+function toNumberBank(event) {
+    event.preventDefault();
+    try {
+        const number = document.getElementById('number').value;
+        NumberBank.push(number);
+        console.log(NumberBank);
+        numberBank.textContent = NumberBank;
+    }
+    catch(error){
+        console.error(error.message);
+        alert('There was an error submitting the form.');
+    }
+}
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById('addNumber').addEventListener('submit', toNumberBank);
+    numberBank = document.getElementById('NumberBank');
+});
