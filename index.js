4a5b202b41a1458d91fd9c465b2ac3a4387b90ee
NKP1229 <113551880+NKP1229@users.noreply.github.com>
2025-04-01 17:00:52 -0400

    sort1 button works and displays on screen

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 // TODO: this file! :)
 const NumberBank = [];
-let numberBank;
+const Odds = [];
+const Evens = [];
+let numberBank, oddNumbers, evenNumbers;
 function toNumberBank(event) {
     event.preventDefault();
     try {
         const number = document.getElementById('number').value;
         NumberBank.push(number);
-        console.log(NumberBank);
+        console.log("NumberBank: ",NumberBank);
         numberBank.textContent = NumberBank;
     }
     catch(error){
@@ -14,7 +16,35 @@ function toNumberBank(event) {
         alert('There was an error submitting the form.');
     }
 }
+function Sort1(){
+    if(NumberBank.length > 0){
+        const number = NumberBank.shift();
+        console.log("popped number: ",number);
+        numberBank.textContent = NumberBank;
+        if(number % 2 === 0){
+            Evens.push(number);
+            console.log("Evens: ", Evens);
+            evenNumbers.textContent = Evens;
+        }
+        else{
+            Odds.push(number);
+            console.log("Odds: ", Odds);
+            oddNumbers.textContent = Odds;
+        }
+    }
+}
+function SortAll(){
+    if(NumberBank.length > 0){
+        
+    }
+}
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById('addNumber').addEventListener('submit', toNumberBank);
     numberBank = document.getElementById('NumberBank');
+    oddNumbers = document.getElementById('oddNumbers');
+    evenNumbers = document.getElementById('evenNumbers');
+    const buttonOne = document.getElementById("sortOne");
+    const buttonAll = document.getElementById("sortAll");
+    buttonOne.addEventListener("click", Sort1);
+    buttonAll.addEventListener("click", SortAll);
 });
